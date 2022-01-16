38050d6346e594c361763119ac47a7be18edd55f
ainaajidian <ainaajidian@gmail.com>
2022-01-16 09:38:06 +0700

    Add Query & Mutation

---
+++ TwittorAPI/GraphQL/Query.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HotChocolate;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TwittorAPI.Dtos;
+using TwittorAPI.Kafka;
+using TwittorAPI.Models;
+
+
+namespace TwittorAPI.GraphQL
+{
+    public class Query
+    {
+        public async Task<IQueryable<Twittor>> GetTwittors(
+            [Service] ProjectTwittorContext context,
+            [Service] IOptions<KafkaSettings> kafkaSettings)
+        {
+            var key = "GetTwittors-" + DateTime.Now.ToString();
+            var val = JObject.FromObject(new { Message = "GraphQL Query GetTwittors" }).ToString(Formatting.None);
+
+            await KafkaHelper.SendMessage(kafkaSettings.Value, "logging", key, val);
+            return context.Twittors;
+        }
+
+        public async Task<IQueryable<UserData>> GetUsers(
+            [Service] ProjectTwittorContext context,
+            [Service] IOptions<KafkaSettings> kafkaSettings)
+        {
+            var key = "getusers-" + DateTime.Now.ToString();
+            var val = JObject.FromObject(new { Message = "GraphQL Query GetUsers" }).ToString(Formatting.None);
+            await KafkaHelper.SendMessage(kafkaSettings.Value, "logging", key, val);
+            return context.Users.Select(p => new UserData()
+            {
+                Id = p.UserId,
+                FullName = p.FullName,
+                Email = p.Email,
+                Username = p.Username
+            });
+
+
+        }
+    }
+}
